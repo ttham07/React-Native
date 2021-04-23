ca23881159d97e6ca90d6731a9602362cf88a9bf
Tommy Tham <tommytham@theShopMBPro.attlocal.net>
2021-04-23 15:35:16 -0400

    Gestures Part 1

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,19 +4,20 @@ import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import Loading from './LoadingComponent';
-​
+import * as Animatable from 'react-native-animatable';
+
 const mapStateToProps = state => {
     return {
         partners: state.partners
     };
 };
-​
+
 class About extends Component {
-​
+
     static navigationOptions = {
         title: 'About Us'
     }
-​
+
     render() {
         const renderPartner = ({item}) => {
             return (
@@ -27,7 +28,7 @@ class About extends Component {
                 />
             );
         };
-​
+
         if (this.props.partners.isLoading) {
             return (
                 <ScrollView>
@@ -42,30 +43,33 @@ class About extends Component {
         if (this.props.partners.errMess) {
             return (
                 <ScrollView>
-                    <Mission />
-                    <Card
-                        title='Community Partners'>
-                        <Text>{this.props.partners.errMess}</Text>
-                    </Card>
+                    <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                        <Mission />
+                        <Card
+                            title='Community Partners'>
+                            <Text>{this.props.partners.errMess}</Text>
+                        </Card>
+                    </Animatable.View>
                 </ScrollView>
             );
         }
-​
             return (  
                 <ScrollView>
-                    <Card title='Mission'>
-                        <Text wrapperStyle={{margin: 10}}>
-                            We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other. 
-                        </Text> 
-                    </Card>
-​
-                    <Card title='Community Partners'>       
-                    <FlatList 
-                        data={this.props.partners.partners}
-                        renderItem={renderPartner}
-                        keyExtractor={item => item.id.toString()}
-                    />
-                    </Card> 
+                    <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                        <Card title='Mission'>
+                            <Text wrapperStyle={{margin: 10}}>
+                                We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other. 
+                            </Text> 
+                        </Card>
+
+                        <Card title='Community Partners'>       
+                        <FlatList 
+                            data={this.props.partners.partners}
+                            renderItem={renderPartner}
+                            keyExtractor={item => item.id.toString()}
+                        />
+                        </Card> 
+                    </Animatable.View>
                 </ScrollView>
             );
         }
--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -174,3 +174,8 @@ export const addComment = comments => ({
     type: ActionTypes.ADD_COMMENT,
     payload: comments
 });
+
+export const deleteFavorite = campsiteId => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: campsiteId
+});
